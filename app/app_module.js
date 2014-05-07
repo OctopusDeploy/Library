e6792a34c4a218508c76fa59baa365ab2fb93a90
nblumhardt <nblumhardt@nblumhardt.com>
2014-05-07 17:18:07 +1000

    Started putting together a gulp-based build pipeline.

---
+++ app/app_module.js
@@ -0,0 +1,1 @@
+var module = angular.module('octopus-library', []);
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var ngmin = require('gulp-ngmin');
+var rev = require('gulp-rev');
+var minifyCss = require('gulp-minify-css');
+var inject = require('gulp-inject');
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+
+gulp.task('scripts-app', ['clean'], function() {
+  return gulp.src(['app/**/*_module.js', 'app/**/*.js'])
+    .pipe(jshint())
+    .pipe(concat('2-app.js'))
+    .pipe(ngmin())
+    .pipe(uglify())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('scripts-vendor', ['clean'], function() {
+  return gulp.src([
+      'bower_components/angular/angular.min.js',
+      'bower_components/angular-route/angular-route.min.js'
+    ])
+    .pipe(concat('1-vendor.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('scripts', ['scripts-app', 'scripts-vendor'], function() {
+});
+
+gulp.task('styles', ['clean'], function() {
+  return gulp.src(['app/**/*.css'])
+    .pipe(concat('app.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('rev', ['scripts', 'styles'], function() {
+  return gulp.src(['build/**/*.css', 'build/**/*.js'])
+    .pipe(rev())
+    .pipe(gulp.dest('dist'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('html-release', ['rev'], function() {
+  return gulp.src('dist/**/*.*')
+    .pipe(inject('app/app.html', {
+      addRootSlash: false,
+      ignorePath: '/dist/'
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html-debug', ['rev'], function() {
+  return gulp.src('build/**/*.*')
+    .pipe(inject('app/app.html', {
+      addRootSlash: false,
+      ignorePath: '/build/'
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src(['build', 'dist'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build', ['html-debug', 'html-release'], function() {
+});
+
+gulp.task('default', ['build'], function() {
+});
