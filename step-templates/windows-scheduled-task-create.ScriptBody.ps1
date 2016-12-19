540dca3b4885fd92541576453324ea3d1e77a5ce
Michael Clayton <mike@kingslandconsulting.co.uk>
2016-12-19 19:26:15 +0000

    #358 - replaced schtasks.exe with custom 'Invoke-CommandLine' error handling

--- step-templates/windows-scheduled-task-create.ScriptBody.ps1
+++ step-templates/windows-scheduled-task-create.ScriptBody.ps1
@@ -1,56 +1,90 @@
-#use http://msdn.microsoft.com/en-us/library/windows/desktop/bb736357(v=vs.85).aspx for API reference
+$ErrorActionPreference = "Stop";
+Set-StrictMode -Version "Latest";
 
-Function Create-ScheduledTask($TaskName,$RunAsUser,$RunAsPassword,$TaskRun, $Arguments, $Schedule,$StartTime,$StartDate){
-    
-    $cmdStartDate = if(StringIsNullOrWhiteSpace($StartDate)){""}else{"/sd $StartDate"}
-    $cmdStartTime = if(StringIsNullOrWhiteSpace($StartTime)){""}else{"/st $StartTime"}
-    $cmdInterval = if(StringIsNullOrWhiteSpace($Interval)){""}else{Validate-ScheduleAndParams($Schedule)}
-    $cmdDuration = if(StringIsNullOrWhiteSpace($Duration)){""}else{"/du $Duration"}
-    
-    $cmd = "'$($TaskRun)'"
-    if($Arguments) {
-        $cmd = "'$TaskRun' '$Arguments'"
-    }
+# use http://msdn.microsoft.com/en-us/library/windows/desktop/bb736357(v=vs.85).aspx for API reference
 
-    $Command = "schtasks.exe /create /ru $RunAsUser /rp '$RunAsPassword' /tn `"$TaskName`" /tr `"$cmd`" /sc $Schedule $cmdStartDate $cmdStartTime /F $cmdInterval $cmdDuration"
-    
-    echo $Command          
-    Invoke-Expression $Command            
-}
+Function Create-ScheduledTask($TaskName,$RunAsUser,$RunAsPassword,$TaskRun, $Arguments, $Schedule,$StartTime,$StartDate)
+{
 
-Function Validate-ScheduleAndParams($Schedule) {
-  switch -Regex ($Schedule) 
+    $argumentList = @();
+    $argumentList += @( "/create" );
+    $argumentList += @( "/ru", $RunAsUser );
+    $argumentList += @( "/rp", $RunAsPassword );
+    $argumentList += @( "/tn", "`"$TaskName`"" );
+    $argumentList += @( "/sc", $Schedule );
+    $argumentList += @( "/f" );
+
+    if( $Arguments )
     {
-      "MINUTE|HOURLY|ONLOGON|ONIDLE"  { "/MO $interval"; break }
-      "WEEKLY|MONTHLY"  { "/ri $interval"; break }
-      "ONCE|ONSTART|ONEVENT"    { ""; break } # We don't currently support providing an XPATH query string at the moment
+        $argumentList += @( "/tr", "`"'$TaskRun' '$Arguments'`"" );
     }
-}
-Function Delete-ScheduledTask($TaskName) {   
-    $Command = "schtasks.exe /delete /s localhost /tn `"$TaskName`" /F"            
-    Invoke-Expression $Command 
+    else
+    {
+        $argumentList += @( "/tr", "'$TaskRun'" );
+    }
+
+    if( -not (StringIsNullOrWhiteSpace($StartDate)) )
+    {
+        $argumentList += @( "/sd", $StartDate );
+    }
+
+    if( -not (StringIsNullOrWhiteSpace($StartTime)) )
+    {
+        $argumentList += @( "/st", $StartTime );
+    }
+
+    if( -not (StringIsNullOrWhiteSpace($Interval)) )
+    {
+        switch -Regex ($Schedule)
+        {
+            "MINUTE|HOURLY|ONLOGON|ONIDLE" {
+                $argumentList += @( "/mo", $Interval );
+            }
+            "WEEKLY|MONTHLY"
+            {
+                $argumentList += @( "/ri", $Interval );
+            }
+            "ONCE|ONSTART|ONEVENT" {
+                # we don't currently support providing an XPATH query string
+                throw new-object System.NotImplementedException("Unsupported schedule option '$Schedule'.");
+            }
+        }
+    }
+
+    if( -not (StringIsNullOrWhiteSpace($Duration)) )
+    {
+        $argumentList += @( "/du", $Duration );
+    }
+
+    Invoke-CommandLine -FilePath     "$($env:SystemRoot)\System32\schtasks.exe" `
+                       -ArgumentList $argumentList;
+
 }
 
-Function Stop-ScheduledTask($TaskName) {  
-    $Command = "schtasks.exe /end /s localhost /tn `"$TaskName`""            
-    Invoke-Expression $Command 
+Function Delete-ScheduledTask($TaskName) {
+    Invoke-CommandLine -FilePath     "$($env:SystemRoot)\System32\schtasks.exe" `
+                       -ArgumentList @( "/delete", "/s", "localhost", "/tn", "`"$TaskName`"", "/F" );
 }
 
-Function Start-ScheduledTask($TaskName) {   
-    $Command = "schtasks.exe /run /s localhost /tn `"$TaskName`""            
-    Invoke-Expression $Command 
+Function Stop-ScheduledTask($TaskName) {
+    Invoke-CommandLine -FilePath     "$($env:SystemRoot)\System32\schtasks.exe" `
+                       -ArgumentList @( "/end", "/s", "localhost", "/tn", "`"$TaskName`"" );
 }
 
-Function Enable-ScheduledTask($TaskName) {  
-    $Command = "schtasks.exe /change /s localhost /tn `"$TaskName`" /ENABLE"            
-    Invoke-Expression $Command 
+Function Start-ScheduledTask($TaskName) {
+    Invoke-CommandLine -FilePath     "$($env:SystemRoot)\System32\schtasks.exe" `
+                       -ArgumentList @( "/run", "/s", "localhost", "/tn", "`"$TaskName`"" );
+}
+
+Function Enable-ScheduledTask($TaskName) {
+    Invoke-CommandLine -FilePath     "$($env:SystemRoot)\System32\schtasks.exe" `
+                       -ArgumentList @( "/change", "/s", "localhost", "/tn", "`"$TaskName`"", "/ENABLE" );
 }
 
 Function ScheduledTask-Exists($taskName) {
-   $schedule = new-object -com Schedule.Service 
-   $schedule.connect() 
+   $schedule = new-object -com Schedule.Service
+   $schedule.connect()
    $tasks = $schedule.getfolder("\").gettasks(0)
-
    foreach ($task in ($tasks | select Name)) {
       #echo "TASK: $($task.name)"
       if($task.Name -eq $taskName) {
@@ -58,14 +92,32 @@ Function ScheduledTask-Exists($taskName) {
          return $true
       }
    }
-
    return $false
-} 
+}
 
 Function StringIsNullOrWhitespace([string] $string)
 {
     if ($string -ne $null) { $string = $string.Trim() }
     return [string]::IsNullOrEmpty($string)
+}
+
+function Invoke-CommandLine
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $FilePath,
+        [Parameter(Mandatory=$false)]
+        [string[]] $ArgumentList,
+        [Parameter(Mandatory=$false)]
+        [string[]] $SuccessCodes = @( 0 )
+    )
+    write-host ($FilePath + " " + ($ArgumentList -join " "));
+    $process = Start-Process -FilePath $FilePath -ArgumentList $ArgumentList -Wait -NoNewWindow -PassThru;
+    if( $SuccessCodes -notcontains $process.ExitCode )
+    {
+        throw new-object System.InvalidOperationException("process terminated with exit code '$($process.ExitCode)'.");
+    }
 }
 
 $taskName = $OctopusParameters['TaskName']
