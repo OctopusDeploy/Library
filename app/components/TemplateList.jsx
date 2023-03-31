1a848ef6f9017f73cbf55f7ce192129d1fb473af
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2023-03-31 10:59:52 +0100

    Parse and limit markdown.

--- app/components/TemplateList.jsx
+++ app/components/TemplateList.jsx
@@ -5,17 +5,25 @@ import moment from "moment";
 import { Link } from "react-router";
 import SlugMaker from "./../services/SlugMaker";
 import PropTypes from "prop-types";
+import { marked } from "marked";
+import DOMPurify from "isomorphic-dompurify";
 
 const displayName = "octopus-library-template-list";
 
 export default class TemplateList extends React.Component {
+  markdown(description) {
+    let firstLine = (description || "").split(/\r?\n|\r|\n/g)[0];
+    let purifiedDescription = DOMPurify.sanitize(firstLine);
+    let markup = marked.parse(purifiedDescription || "");
+    return { __html: markup };
+  }
+
   render() {
     let templateList = this.props.templateList.map((item, index) => {
       let lc = this.props.filterText.toLowerCase();
       if (item.Name.toLowerCase().indexOf(lc) === -1 && (item.Description === null || item.Description.toLowerCase().indexOf(lc) === -1)) {
         return;
       }
-      let formattedExportedAt = moment(item.ExportedAt).calendar();
       let friendlySlug = SlugMaker.make(item.Name);
       return (
         <li className={"item-summary " + item.ScriptClass} key={index + "." + item.Name}>
@@ -23,7 +31,7 @@ export default class TemplateList extends React.Component {
           <h4 key={index + "." + item.Name + ".0"}>
             <Link to={`/step-templates/${item.Id}/${friendlySlug}`}>{item.Name}</Link>
           </h4>
-          <p className="faint">{item.Description}</p>
+          <div className="faint" dangerouslySetInnerHTML={this.markdown(item.Description)}></div>
         </li>
       );
     });
