e44663ad59a434be600162773eb743b3e08f18cc
bullman <brad@ullman.com>
2024-03-19 19:37:50 -0700

    make invoke-pestertests work with globally installed pester

--- step-templates/tests/Invoke-PesterTests.ps1
+++ step-templates/tests/Invoke-PesterTests.ps1
@@ -4,11 +4,12 @@ Set-StrictMode -Version "Latest";
 $thisScript = $MyInvocation.MyCommand.Path;
 $thisFolder = [System.IO.Path]::GetDirectoryName($thisScript);
 $rootFolder = [System.IO.Path]::GetDirectoryName($thisFolder);
-$testFolder = [System.IO.Path]::Combine($thisFolder, "scripts");
 
 $testableScripts = @(
-    "windows-scheduled-task-create.ScriptBody.ps1"
+    "windows-scheduled-task-create.ScriptBody.ps1",
+    "sql-backup-database.ScriptBody.ps1"
 );
+
 foreach( $script in $testableScripts )
 {
     $filename = [System.IO.Path]::Combine($rootFolder, $script);
@@ -19,11 +20,14 @@ foreach( $script in $testableScripts )
     . $filename;
 }
 
-$packagesFolder = $thisFolder;
-$packagesFolder = [System.IO.Path]::GetDirectoryName($packagesFolder);
-$packagesFolder = [System.IO.Path]::GetDirectoryName($packagesFolder);
-$packagesFolder = [System.IO.Path]::Combine($packagesFolder, "packages");
-
-Import-Module -Name ([System.IO.Path]::Combine($packagesFolder, "Pester.3.4.3\tools\Pester"));
+try {
+  $packagesFolder = $thisFolder;
+  $packagesFolder = [System.IO.Path]::GetDirectoryName($packagesFolder);
+  $packagesFolder = [System.IO.Path]::GetDirectoryName($packagesFolder);
+  $packagesFolder = [System.IO.Path]::Combine($packagesFolder, "packages");
+  Import-Module -Name ([System.IO.Path]::Combine($packagesFolder, "Pester.3.4.3\tools\Pester")) -ErrorAction Stop
+} catch {
+  Import-Module Pester
+}
 
 Invoke-Pester;
