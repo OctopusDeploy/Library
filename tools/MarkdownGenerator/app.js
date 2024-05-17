db982aa968c6a0636b7b041316012a3639f76b7a
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2024-05-17 13:21:42 +0100

    Initial version

---
+++ tools/MarkdownGenerator/app.js
@@ -0,0 +1,85 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+
+const templatePath = '../../step-templates';
+const distributionPath = '../../integrations';
+
+const files = await fs.readdir(templatePath);
+const categories = {};
+
+await cleanOutputFolder();
+
+for (let file of files) {
+    const json = await getTemplateData(file);
+
+    if (json == null) {
+        continue;
+    }
+
+    const category = getSafeCategory(json);
+
+    await createCategory(category);
+
+    categories[category].templates.push({
+        id: json.Id,
+        name: json.Name
+    });
+
+    await createMarkdown(category, json);
+}
+
+console.log(categories);
+
+async function cleanOutputFolder() {
+    await fs.rmdir(distributionPath, { recursive: true });
+}
+
+async function getTemplateData(file) {
+    const filePath = `${templatePath}/${file}`;
+    const ext = path.extname(filePath);
+
+    if (ext != '.json') {
+        return null;
+    }
+
+    console.log(ext, filePath);
+
+    const fileText = await fs.readFile(filePath, { encoding: 'utf8' });
+    return JSON.parse(fileText);
+}
+
+function getSafeCategory(data) {
+    const category = data.Category;
+    if (category == null || category.length == 0) {
+        console.warn(`There is no category for ${data.Name}. Using 'other'.`);
+        return 'other';
+    }
+
+    return category.toLowerCase();
+}
+
+async function createCategory(category) {
+    const categoryExists = categories[category] != null;
+
+    if (categoryExists) {
+        categories[category].count++;
+    } else {
+        categories[category] = { count: 1, templates: []}
+    }
+
+    await fs.mkdir(`${distributionPath}/${toSlug(category)}`, { recursive: true });
+}
+
+async function createMarkdown(category, data) {
+    const content = `---
+    todo: frontmatter
+---
+
+`;
+
+    await fs.writeFile(`${distributionPath}/${toSlug(category)}/${toSlug(data.Name)}.md`, content)
+}
+
+function toSlug(text) {
+    return encodeURIComponent(text.toLowerCase().replace(/ - /g, ' ').replace(/ /g, '-'));
+}
